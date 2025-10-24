37475a2e5cc3083bff59a8afc95875b4e730d31a
Rhamses <rhamses.soares@gmail.com>
2025-10-24 15:03:02 -0300

    Ajustes de edge

--- site/src/lib/prisma.ts
+++ site/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { PrismaD1 } from "@prisma/adapter-d1";
 
 const PRISMA_CACHE = Symbol.for("domain-alert.prisma");
